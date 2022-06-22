a812156a26e83f0857179c771764ad4e3dc6fcd1
Martin <Martin@DESKTOP-KCL3THO>
2022-06-22 21:05:30 +0200

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,9 @@
+﻿Dictionary<string, string> countries = new Dictionary<string, string>();
+
+countries.Add("UK", "London");
+countries.Add("US", "Washington");
+countries.Add("ČR", "Prague");
+
+foreach (KeyValuePair<string, string> country in countries)
+    Console.WriteLine($"{country.Key} : {country.Value}");
+Console.ReadLine();
